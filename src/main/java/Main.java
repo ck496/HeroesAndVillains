10354ddd1de3c480c827b02ce39c8d80d0d03a5e
ck496 <chriskurian24@gmail.com>
2020-04-07 20:41:29 -0700

    Created base folder structure and included .gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Heros and Villans Universe! "
+                + "\n\nWhich side will you choose?? \nWho will emerge "
+                + "victorious? \nWhat will be the fate of the universe?"
+                + "\n\nFind out soon..........");
+        
+        
+
+    }
+
+}
