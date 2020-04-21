f05834aa118241f08c9eb3b6009284936428b3cc
ck496 <chriskurian24@gmail.com>
2020-04-21 02:21:14 -0700

    Create base/lair class for villans and heros with builder pattern

---
+++ src/main/java/Base.java
@@ -0,0 +1,300 @@
+package main.java;
+
+import java.util.NoSuchElementException;
+import java.util.Vector;
+
+
+/**
+ * Base class creates the bases for the villians and heros within the 
+ * words. There can be a total of 5  m 
+ * @author chris
+ *
+ */
+public class Base {
+
+    public String[] stateList = { "Lair", "Base", "Open" , "Closed"};
+    private Vector<Character> members = new Vector<Character>(5);
+
+    private String name;
+    private String state;
+    private int totalMembers;
+
+    public Base(Builder builder) {
+        this.members = builder.members;
+        this.name = builder.name;
+        this.totalMembers = builder.totalMembers;
+        this.setState(builder.state);
+    }
+
+    /**
+     * @return the members
+     */
+    public Vector<Character> getMembers() {
+        return members;
+    }
+
+    /**
+     * @param members the members to set
+     */
+    public void setMembers(Vector<Character> members) {
+        this.members = members;
+        this.totalMembers = members.size();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the charecterNum
+     */
+    public int getTotalMembers() {
+        return totalMembers;
+    }
+    
+
+    /**
+     * Add Characters to vector.
+     * @param ch
+     * @return Builder
+     * @throws IllegalAccessException 
+     */
+    public void addMember(Character ch) {
+        
+        if(totalMembers >= 5) {
+            throw new IndexOutOfBoundsException("Base is at Max Capacity (5) right now, "
+                    + "cant add new members");
+        }
+        
+        if(ch.isHero() && state.equals("Lair")) {
+            throw new IllegalArgumentException("Hero cant be added to a Villians Lair");
+        }
+        else if(!ch.isHero() && state.equals("Base")) {
+            throw new IllegalArgumentException("Villian cant be added to a Heros Base");
+        }
+        
+        this.members.add(ch);
+        this.totalMembers++;
+       
+    }
+    
+    
+    /**
+     * Remove Dead Characters from base.
+     * @throws ArithmeticException 
+     */
+    public void removeDead() {
+        if(totalMembers == 0) {
+            throw new ArithmeticException("Base is empty, no one to remove!");
+        }
+        boolean hasDead = false;
+        Vector<Character> tmp = (Vector<Character>) members.clone();
+        for(Character ch : tmp) {
+            if(ch.getState().equals("Dead")) {
+                members.remove(ch);
+                totalMembers--;
+                hasDead = true;
+                System.out.println("REMOVED DEAD:  " +ch.getName() + " from " + this.name);
+                //ch.print();
+            }
+        }
+        if(hasDead) {
+            print();
+        }
+        else {
+            System.out.println("No Dead members in " + this.name );
+        }
+        
+    }
+    
+    /**
+     * 
+     * Remove Member given a name and their power.
+     * @param name
+     * @param power
+     */
+    public void removeMember(String name, String power) {
+        if(totalMembers == 0) {
+            throw new ArithmeticException("Base is empty, no one to remove!");
+        }
+        boolean inBase = false;
+        for(Character ch : members) {
+            if(ch.getName().equals(name) && ch.getPower().equals(power)) {
+                members.remove(ch);
+                totalMembers--;
+                System.out.println("REMOVED:  " +ch.getName() + " from " + this.name);
+                ch.print();
+                inBase = true;
+                break;
+                
+            }
+        }
+        
+        if(!inBase) {
+            throw new NoSuchElementException("No member with the name '"+ name 
+                    + "' and power: '" + power + "' exists in " + this.name);
+        }
+    }
+    
+    
+    
+    
+    /**
+     * Gets the strongest.
+
+     * @return strongest
+     */
+    public Character getStrongestMember() {
+        if(totalMembers == 0) {
+            throw new ArithmeticException("Base is empty, no one to remove!");
+        }
+        Character strongest = members.get(0);
+        
+        for(Character ch : members) {
+            if(ch.getLevel() > strongest.getLevel()) {
+                strongest = ch;
+            }
+        } 
+        
+        System.out.println(this.name +" STRONGEST Member: " + strongest.getName() 
+                        + ", lvl: " + strongest.getLevel());
+
+        return strongest;
+    }
+    
+    /**
+     * Gets the Weakest.
+
+     * @return Weakest
+     */
+    public Character getWeakestMember() {
+        if(totalMembers == 0) {
+            throw new ArithmeticException("Base is empty, no one to remove!");
+        }
+        Character weakest = members.get(0);
+        
+        for(Character ch : members) {
+            if(ch.getLevel() < weakest.getLevel()) {
+                weakest = ch;
+            }
+        }
+        
+        System.out.println(this.name +" WEAKEST Member: " + weakest.getName()
+                        + ", lvl: " + weakest.getLevel());
+        return weakest;
+    }
+    
+    
+    public void print() {
+        System.out.println("---- Base Detials ----");
+        System.out.println("\t[Name]: " + this.name + "\n\t[State]: " 
+                        + this.state + "\n\t[Total Memebrs]: " + this.totalMembers 
+                        + "\n\t[Memeber Detils]: ");
+        if(totalMembers!=0) {
+            int i=0;
+            for(Character ch :members) {
+                i++;
+                System.out.println("\n         [MEMBER " +i +"]");
+                ch.print();
+            }
+        }
+        else {
+            System.out.println("0 memebers");
+        }
+    }
+
+
+    /**
+     * Constructs the base objects using builder pattern
+     */
+    public static class Builder {
+        private String name;
+        private String state = "";
+        private int totalMembers;
+        Vector<Character> members = new Vector<Character>(5);
+
+        /**
+         * 
+         * @param name
+         * @return Builder
+         */
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        /**
+         * sets the state;
+         * @param state
+         * @return
+         */
+        public Builder setState(String state) {
+            this.state = state;
+            return this;
+        }
+
+        /**
+         * 
+         * @param memberVector
+         * @return Builder
+         */
+        public Builder setMemberVector(Vector<Character> chArr) {
+            for(Character ch : chArr) {
+                Character newCharacter = new Character.Builder().isHero(ch.isHero())
+                                       .setName(ch.getName()).setState(ch.getState())
+                                       .setLevel(ch.getLevel()).setPower(ch.getPower())
+                                       .constructCharacter();
+                members.add(newCharacter);
+            }
+            return this;
+        }
+        
+        /**
+         * Add Characters to vector.
+         * @param ch
+         * @return Builder
+         */
+        public Builder addMemeber(Character ch) {
+            this.members.add(ch);
+            return this;
+        }
+
+        /**
+         * Constructs the base objects using builder pattern
+         * 
+         * @return Base
+         */
+        public Base baseConstruct() {
+            this.totalMembers = this.members.size();
+            if(this.state.equals("")) {
+                this.state = "Open";
+            }
+            return new Base(this);
+        }
+
+    }
+
+}
--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Character {
 
 
-     protected String[] stateListV = { "Attack", "Create", "Fight", "Recover", "Move", "Start", "Dead" };
+     protected String[] stateListV = { "Attack", "Create", "Fight", "Recover", "Move", "Dead" };
 
-     protected String[] stateListH = { "Protect", "Create", "Fight", "Recover", "Move", "Start", "Dead" };
+     protected String[] stateListH = { "Protect", "Create", "Fight", "Recover", "Move", "Dead" };
 
      
     private String name;
@@ -93,6 +93,13 @@ public class Character {
      * @param health the health to set
      */
     public void setHealth(int health) {
+        if(health == 0) {
+            this.state = "Dead";
+            this.level = 0;
+            System.out.println("R.I.P " + this.name + ", You "
+                    + "have fought the good fight,"
+                    + "\nYou will be avenged!");
+        }
         this.health = health;
     }
 
@@ -131,7 +138,7 @@ public class Character {
             type = "Villain";
         }
         
-        System.out.println("------ "+type + " Detials ------\n\t[Name]: " + this.name 
+        System.out.println("------ "+type + " Details ------\n\t[Name]: " + this.name 
                 + "\n\t[Health]: " + this.health + "\n\t[State]: " + this.state 
                 + "\n\t[Power]: " + this.power + "\n\t[Level]: " + this.level + 
                 "\n\t[+Strength]: " + this.strength + "\n\t[-Weakness]: " + this.weakness);
@@ -149,7 +156,7 @@ public class Character {
         private String[] villanLast = { "Destroyer", "Creator", "Worshiper", "Orlamünde", "Reaper", "Collector", "Krampus",
                "Opressor", "Poltergeist", "Hansel", "Grethel", "Elwetritschen", "Vader", "Bone", "Terror" };
 
-        private String[] heroFirst = { "Captian", "Sonic", "Super", "Beast", "Crystal", "Speed", "Light", "Power", "Lord",
+        private String[] heroFirst = { "Captain", "Sonic", "Super", "Beast", "Crystal", "Speed", "Light", "Power", "Lord",
                "Vision", "Hell", "Soul", "Doctor", "Galactic", "Aqua" };
 
         private String[] heroLast = { "Surfer", "Boy", "Women", "Man", "Girl", "Avenger", "Protector", "Defender", "Jedi",
@@ -157,26 +164,16 @@ public class Character {
         
         private String[] powerList = { "Fire", "Water", "Earth", "Electric", "Psychic", "Dark", "Fighting" };
         
-        private String name;
+        private String name = "";
         private boolean isHero;
-        private String state;
-        private String power;
+        private String state ="";
+        private String power = "";
         private String strength;
         private String weakness;
-        private int level;
+        private int level = 0;
         private int health;
         
-        
-        /**
-         * Allows you to pick if its a hero or not.
-         * @param isHero
-         * @return
-         */
-        public Builder isHero(boolean isHero){
-            this.isHero = isHero;
-            return this;
-            
-        }
+        //------private method for builder------
         /**
          * Function uses a random generator to pick a name
          * based on hero or villain.
@@ -203,14 +200,11 @@ public class Character {
         /**
          * @param power the power to set
          */
-        private void setPower() {
+        private void setRanPower() {
             int powerIndex = randomGen(0, powerList.length -1);
             this.power = powerList[powerIndex];
         }
         
-        /**
-         * @param weaknes the weakness to set
-         */
         /**
          * @param weakness the weakness to set
          */
@@ -238,7 +232,6 @@ public class Character {
             }
         }
       
-
         /**
          * @param strength the strength to set
          */
@@ -266,17 +259,84 @@ public class Character {
             }
         }
         
+        
+        //----Public methods available to users----
+        
+        /**
+         * Allows you to pick if its a hero or not.
+         * @param isHero
+         * @return
+         */
+        public Builder isHero(boolean isHero){
+            this.isHero = isHero;     
+            return this;
+            
+        }
+        /**
+         * Lets you pick a custom name.
+         * @param aName
+         * @return
+         */
+        public Builder setName(String aName) {
+            this.name = aName;
+            return this;
+        }
+        
+        public Builder setState(String state) {
+            this.state = state;
+            return this;
+        }
+        /**
+         * @param power the power to set
+         */
+        public Builder setPower(String aPower) {
+           
+            this.power = aPower;
+            return this;
+        }
+
+        /**
+         * Set The level;
+         * @param lvl
+         * @return
+         */
+        public Builder setLevel(int lvl) {
+           this.level = lvl;
+            return this;
+        }
+        
+        
         /**
          * constructCharacter() builds and returns a Character obj.
          * 
          * @return Character
          */
         public Character constructCharacter() {
-            this.state = "Start";
-            this.level=1;
+            //if variables were not set by user set them to 
+            //randomly generated values 
+            
+            if(this.name.equals("")) {
+                setName();
+            }
+            
+            if(this.state.equals("")){
+                if(isHero) {
+                    this.state = "Protect";
+                }
+                else {
+                    this.state = "Attack";
+                }
+            }
+            
+            if(this.level == 0) {
+                this.level = 1;
+            }
+            
+            if(this.power.equals("")) {
+                setRanPower();
+            }
+            
             this.health=100;
-            setName();
-            setPower();
             setStrength();
             setWeakness();
             return new Character(this);
